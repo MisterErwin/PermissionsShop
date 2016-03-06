41faf6ad9a84feb06279a282028a9efdcfa992a2
j0ach1mmall3 <business.j0ach1mmall3@gmail.com>
2016-03-06 14:32:31 +0100

    Added files via upload

---
+++ src/java/com/j0ach1mmall3/permissionsshop/api/Category.java
@@ -0,0 +1,28 @@
+package com.j0ach1mmall3.permissionsshop.api;
+
+import com.j0ach1mmall3.jlib.inventory.GuiItem;
+
+public class Category extends CategoryPackageHolder {
+	private final String permission;
+	private final double price;
+	private final GuiItem item;
+
+    public Category(String identifier, String permission, double price, GuiItem item, int guiSize) {
+        super(identifier, item.getItem().getItemMeta().getDisplayName(), guiSize);
+        this.permission = permission;
+        this.price = price;
+        this.item = item;
+    }
+
+    public String getPermission() {
+        return this.permission;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public GuiItem getItem() {
+        return this.item;
+    }
+}
